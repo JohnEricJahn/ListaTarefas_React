dd1c8ea3d33f67ae42208f2dbb4835d67b97886e
FrostPriest <johnericjahn@gmail.com>
2020-05-11 10:37:09 -0300

    Criando_Estrutura

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/jsx-filename-extension': 'off',
+    'linebreak-style': 'off',
+    'react/state-in-constructor': 'off',
+  },
+};
